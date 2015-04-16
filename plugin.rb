0c9dee6310d765634a691b2ce9c05539189566ce
Neil Lalonde <neillalonde@gmail.com>
2015-04-16 15:00:09 -0400

    Add metadata so it loads in admin

--- plugin.rb
+++ plugin.rb
@@ -1,2 +1,7 @@
+# name: discourse-twittercommunity
+# about: Customizations for twittercommunity.com
+# version: 0.1
+# authors: Neil Lalonde
+
 register_asset "javascripts/discourse/templates/connectors/poster-avatar-bottom/under-avatar.js.handlebars"
 register_asset "stylesheets/twittercommunity.scss"
